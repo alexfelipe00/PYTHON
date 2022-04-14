4bfd9171f5f5ecd241bf3d5e64766cdcf4461d0d
Alex Felipe de Souza <56352969+alexfelipe00@users.noreply.github.com>
2022-04-14 02:49:58 -0400

    Estudos python

---
+++ ex064.py
@@ -0,0 +1,8 @@
+pessoas = {'nome': 'Alex', 'idade': '24', 'sexo': 'M'}
+print(pessoas)
+
+print(f"O {pessoas['nome']} do sexo {pessoas['sexo']} tem {pessoas['idade']} anos de idade")
+
+print(f"{pessoas.keys()}")
+print(f"{pessoas.values()}")
+print(f"{pessoas.items()}")
