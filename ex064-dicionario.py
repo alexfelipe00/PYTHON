1b6f3c41686f9627acfc481c0eac46363c6be2c7
Alex Felipe de Souza <56352969+alexfelipe00@users.noreply.github.com>
2022-04-23 16:07:51 -0400

    estudo dicionarios

--- ex064-dicionario.py
+++ ex064-dicionario.py
@@ -3,6 +3,17 @@ print(pessoas)
 
 print(f"O {pessoas['nome']} do sexo {pessoas['sexo']} tem {pessoas['idade']} anos de idade")
 
+del pessoas['sexo']
+pessoas['nome'] = 'Marcio'
+
+pessoas['peso'] = 71
+
 print(f"{pessoas.keys()}")
 print(f"{pessoas.values()}")
 print(f"{pessoas.items()}")
+
+for k in pessoas.keys():
+    print(f"{k}")
+
+for k, v in pessoas.items():
+    print(f"{k} = {v}")
---
+++ ex065-dicionario2.py
@@ -0,0 +1,7 @@
+estado1 = {'nome': 'Mato Grosso do Sul', 'sigla': 'MS'}
+estado2 = {'nome': 'Paraná', 'sigla': 'PR'}
+
+brasil = []
+brasil.append(estado1)
+
+print(brasil)
